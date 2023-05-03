5965acbd4660431d2388076db6116078b5c8cc9f
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-03 22:27:49 +0530

    .

--- Arrays/Second_Largest_GFG.cpp
+++ Arrays/Second_Largest_GFG.cpp
@@ -1,23 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute force	O(nlogn)
+// class Solution{
+// public:	
+// 	// Function returns the second
+// 	// largest elements
+// 	int print2largest(int arr[], int n) {
+// 	    sort(arr, arr + n, greater<int>());
+// 	    int i = 1;
+// 	    while (i < n)
+// 	    {
+// 	        if (arr[i] != arr[i - 1])
+// 	            return arr[i];
+// 	        i++;
+// 	    }
+// 	    return -1;
+// 	}
+// };
+
+// Better approach	O(2n)
+// class Solution{
+// public:	
+// 	// Function returns the second
+// 	// largest elements
+// 	int print2largest(int arr[], int n) {
+// 	    int lg = 0, slg = 0;
+// 	    for (int i = 0; i < n; i++) 
+// 	        if (lg < arr[i])
+// 	            lg = arr[i];
+// 	    for (int i = 0; i < n; i++)
+// 	        if (slg < arr[i] && arr[i] != lg)
+// 	            slg = arr[i];
+// 	    if (slg == 0)
+// 	        return -1;
+// 	    return slg;
+// 	}
+// };
+
+// Optimal approach	O(n)
 class Solution{
 public:	
 	// Function returns the second
 	// largest elements
 	int print2largest(int arr[], int n) {
-	    sort(arr, arr + n, greater<int>());
-	    int i = 1;
-	    while (i < n)
+	    int lg = -1, slg = -1;
+	    for (int i = 0; i < n; i++)
 	    {
-	        if (arr[i] != arr[i - 1])
-	            return arr[i];
-	        i++;
+	        if (arr[i] > lg)
+	        {
+	            slg = lg;
+	            lg = arr[i];
+	        }
+	        else if (arr[i] < lg && arr[i] > slg)
+	            slg = arr[i];
 	    }
-	    return -1;
+	    return slg;
 	}
 };
-
 
 int main()
 {
