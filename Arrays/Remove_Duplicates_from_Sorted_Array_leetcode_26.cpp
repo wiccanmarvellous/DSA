8fa98beb20c6a3799f568bfbc4dd1b7bc6edb57a
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-06 00:01:42 +0530

    .

--- Arrays/Remove_Duplicates_from_Sorted_Array_leetcode_26.cpp
+++ Arrays/Remove_Duplicates_from_Sorted_Array_leetcode_26.cpp
@@ -2,24 +2,35 @@
 using namespace std;
 
 // Approach 1
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        set<int> s(begin(nums), end(nums));
-        nums.clear();
-        for (auto it : s)
-            nums.push_back(it);
-        return s.size();
-    }
-};
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         set<int> s(begin(nums), end(nums));
+//         nums.clear();
+//         for (auto it : s)
+//             nums.push_back(it);
+//         return s.size();
+//      // OR
+//         set<int> s;
+//         for (auto it : nums)
+//             s.insert(it);
+//         int i = 0;
+//         for (auto it : s)
+//             nums[i++] = it;
+//         return i;
+//     }
+// };
 
 // Approach 2
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates(vector<int> &nums)
+    {
         int i, j = 1, n = nums.size();
         for (int i = 1; i < n; i++)
-            if (nums[i] != nums[i - 1]) {
+            if (nums[i] != nums[i - 1])
+            {
                 nums[j] = nums[i];
                 j++;
             }
@@ -27,7 +38,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     vector<int> v{1, 1, 1, 2, 2, 3, 3};
     Solution obj;
     int k = obj.removeDuplicates(v);
