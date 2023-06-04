4ce655ea0f6d1b9e5f18b495db2a5ed43cec5bc1
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-04 20:16:25 +0530

    feat: create Rotate_Image_leetcode_48.cpp

---
+++ Arrays/Medium/Rotate_Image_leetcode_48.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < j)
+                    swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
