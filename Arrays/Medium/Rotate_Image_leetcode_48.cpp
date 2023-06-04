c0397e4f5c2aad72fadece73a8dae8f18719503a
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-04 21:28:28 +0530

    update Rotate_Image_leetcode_48.cpp

--- Arrays/Medium/Rotate_Image_leetcode_48.cpp
+++ Arrays/Medium/Rotate_Image_leetcode_48.cpp
@@ -16,6 +16,7 @@ public:
     }
 };
 
+// Optimal       TC O(((n / 2) * (n / 2)) + (n * (n / 2)))     SC O(1)
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -27,5 +28,17 @@ public:
                     swap(matrix[i][j], matrix[j][i]);
             }
         }
+    }
+};
+// OR
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n - 1; i ++)
+            for (int j = i + 1; j < n; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        for (int i = 0; i < n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
     }
 };
