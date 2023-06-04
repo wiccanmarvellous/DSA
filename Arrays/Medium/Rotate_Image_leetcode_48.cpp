72c2053338d6f58f63f0939af9fccd60f186a1f3
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-04 20:30:12 +0530

    update Rotate_Image_leetcode_48.cpp

--- Arrays/Medium/Rotate_Image_leetcode_48.cpp
+++ Arrays/Medium/Rotate_Image_leetcode_48.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute        TC O(n * n)     SC O(n * n)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> ans(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = matrix[n - 1 - j][i];
+            }
+        }
+        matrix = ans;
+    }
+};
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
