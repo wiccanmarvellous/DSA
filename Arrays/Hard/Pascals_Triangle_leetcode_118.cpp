8fcd7e940eaa9a2ca27fc5341d856dac4f3e188c
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-07 22:00:13 +0530

    update Pascals_Triangle_leetcode_118.cpp

--- Arrays/Hard/Pascals_Triangle_leetcode_118.cpp
+++ Arrays/Hard/Pascals_Triangle_leetcode_118.cpp
@@ -22,6 +22,31 @@ public:
     }
 };
 
+class Solution {
+public:
+
+    int nCr(int n, int r) {
+    long long res = 1;
+
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for (int row = 1; row <= numRows; row++) {
+        vector<int> tempLst; 
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+    }
+};
+
 int main() {
     int numRows;
     cin >> numRows;
