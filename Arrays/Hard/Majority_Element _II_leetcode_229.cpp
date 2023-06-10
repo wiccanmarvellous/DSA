53cbd0f0ea9a22a3655587eb2d50e549a8abf7d5
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-10 23:45:56 +0530

    update 'Majority_Element _II_leetcode_229.cpp'

--- Arrays/Hard/Majority_Element _II_leetcode_229.cpp
+++ Arrays/Hard/Majority_Element _II_leetcode_229.cpp
@@ -27,3 +27,19 @@ public:
         return ans;
     }
 };
+
+// Better       TC O(n)     SC O(n)
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        unordered_map<int, int> dict;
+        for (int i = 0; i < n; i++)
+            dict[nums[i]]++;
+        for (auto it : dict)
+            if (it.second > n / 3)
+                ans.push_back(it.first);
+        return ans;
+    }
+};
