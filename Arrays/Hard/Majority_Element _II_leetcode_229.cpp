2e4d0c7e44c6dfb11d910a6c0c74d3cb1c291201
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-11 20:10:34 +0530

    update 'Majority_Element _II_leetcode_229.cpp'

--- Arrays/Hard/Majority_Element _II_leetcode_229.cpp
+++ Arrays/Hard/Majority_Element _II_leetcode_229.cpp
@@ -43,3 +43,42 @@ public:
         return ans;
     }
 };
+
+// Optimal      TC O(n)     SC O(1)
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int count1 = 0, count2 = 0, ele1 = INT_MIN, ele2 = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count1 == 0 && nums[i] != ele2) {
+                ele1 = nums[i];
+                count1 = 1;
+            }
+            else if (count2 == 0 && nums[i] != ele1) {
+                ele2 = nums[i];
+                count2 = 1;
+            }
+            else if (nums[i] == ele1)
+                count1++;
+            else if (nums[i] == ele2)
+                count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == ele1)
+                count1++;
+            else if (nums[i] == ele2)
+                count2++;
+        }
+        if (count1 > nums.size() / 3)
+            ans.push_back(ele1);
+        if (count2 > nums.size() / 3)
+            ans.push_back(ele2);
+        return ans;
+    }
+};
