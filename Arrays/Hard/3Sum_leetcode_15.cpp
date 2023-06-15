8738a22df353669fdd7058a178f86242a03743e0
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-15 19:16:19 +0530

    update 3Sum_leetcode_15.cpp

--- Arrays/Hard/3Sum_leetcode_15.cpp
+++ Arrays/Hard/3Sum_leetcode_15.cpp
@@ -21,3 +21,54 @@ public:
         return ans;
     }
 };
+
+// Better       TC O(n * n * log m)     SC O(n + n)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        for (int i = 0; i < nums.size(); i++) {
+            set<int> hashset;
+            for (int j = i + 1; j < nums.size(); j++) {
+                int third = -(nums[i] + nums[j]);
+                if (hashset.find(third) != hashset.end()) {
+                    vector<int>temp{nums[i], nums[j], third};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[j]);
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
+// Optimal      TC O(n * n)       SC O(n)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] +nums[j] +nums[k];
+                if (sum < 0)
+                    j++;
+                else if (sum > 0)
+                    k--;
+                else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
