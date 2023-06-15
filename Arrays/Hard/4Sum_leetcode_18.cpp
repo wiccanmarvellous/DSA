7e940f31a6699dea1c81d7f52eb3571781696dfd
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-15 20:13:09 +0530

    update 4Sum_leetcode_18.cpp

--- Arrays/Hard/4Sum_leetcode_18.cpp
+++ Arrays/Hard/4Sum_leetcode_18.cpp
@@ -28,7 +28,7 @@ public:
     }
 };
 
-// Better       TC O(n * n * n)     SC O(n + n)
+// Better       TC O(n * n * n * log m)     SC O(n + n)
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -50,6 +50,41 @@ public:
             }
         }
         vector<vector<int>> ans(tset.begin(), tset.end());
+        return ans;
+    }
+};
+
+// Optimal      TC O(n * n * n)     SC O(n)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum < target)
+                        k++;
+                    else if (sum > target)
+                        l--;
+                    else {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                }
+            }
+        }
         return ans;
     }
 };
