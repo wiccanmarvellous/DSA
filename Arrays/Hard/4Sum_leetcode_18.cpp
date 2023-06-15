094569bb97828657eb0afb2a5a5c5130b94c3794
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-15 19:35:53 +0530

    update 4Sum_leetcode_18.cpp

--- Arrays/Hard/4Sum_leetcode_18.cpp
+++ Arrays/Hard/4Sum_leetcode_18.cpp
@@ -11,13 +11,41 @@ public:
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
                     for (int l = k + 1; l < n; l++) {
-                        if (i != j && j != k && k != l && l != i && 
-                        (nums[i] + nums[j] + nums[k] + nums[l] == target)) {
+                        long long sum = nums[i] + nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+                        if (sum == target) {
                             vector<int> temp{nums[i], nums[j], nums[k], nums[l]};
                             sort(temp.begin(), temp.end());
                             tset.insert(temp);
                         }
                     }
+                }
+            }
+        }
+        vector<vector<int>> ans(tset.begin(), tset.end());
+        return ans;
+    }
+};
+
+// Better       TC O(n * n * n)     SC O(n + n)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        set<vector<int>> tset;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                set<int> hashset;
+                for (int k = j + 1; k < n; k++) {
+                    int fourth = target - (nums[i] + nums[j] + nums[k]);
+                    if (hashset.find(fourth) != hashset.end()) {
+                        vector<int> temp{nums[i], nums[j], nums[k], fourth};
+                        sort(temp.begin(), temp.end());
+                        tset.insert(temp);
+                    }
+                    else
+                        hashset.insert(nums[k]);
                 }
             }
         }
