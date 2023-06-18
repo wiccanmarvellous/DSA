0b89288592b26af88ed20a1029869b426596bd07
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-18 10:38:41 +0530

    optimal

--- Arrays/Hard/Subarray_with_given_XOR_Interview_Bit.cpp
+++ Arrays/Hard/Subarray_with_given_XOR_Interview_Bit.cpp
@@ -5,7 +5,24 @@ class Solution {
     int solve(vector<int> &A, int B){};
 };
 
-// Brute        TC O(n * n)
+// Brute        TC O(n * n * n)
+int Solution::solve(vector<int> &A, int B) {
+    int count = 0;
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = i; j < A.size(); j++) {
+            int X = 0;
+            for (int k = i; k <= j; k++) {
+                X = X ^ A[k];
+            }
+            if (X == B)
+                count++;
+        }
+    }
+    return count;
+}
+
+
+// Better        TC O(n * n)
 int Solution::solve(vector<int> &A, int B) {
     int X = 0, count = 0;
     for (int i = 0; i < A.size(); i++) {
@@ -19,4 +36,15 @@ int Solution::solve(vector<int> &A, int B) {
     return count;
 }
 
-
+// Optimal      TC O(n)     SC O(n)
+int Solution::solve(vector<int> &A, int B) {
+    map<int, int> hash;
+    hash[0] = 1;
+    int X = 0, count = 0;
+    for (int i = 0; i < A.size(); i++) {
+        X = X ^ A[i];
+        count += hash[X ^ B];
+        hash[X]++;
+    }
+    return count;
+}
