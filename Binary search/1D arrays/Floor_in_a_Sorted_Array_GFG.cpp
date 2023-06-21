5a952e8ae4a070233917cc8d6ed1a2aca7f6769f
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-21 20:52:23 +0530

    update Floor_in_a_Sorted_Array_GFG.cpp

--- Binary search/1D arrays/Floor_in_a_Sorted_Array_GFG.cpp
+++ Binary search/1D arrays/Floor_in_a_Sorted_Array_GFG.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 //      TC O(log n)
-class Solution{
-  public:
-    int findFloor(vector<long long> v, long long n, long long x){
+class Solution
+{
+public:
+    int findFloor(vector<long long> v, long long n, long long x)
+    {
         if (x < v[0])
             return -1;
         int first = 0, last = n - 1;
-        while (first <= last) {
+        while (first <= last)
+        {
             int mid = (first + last) / 2;
             if (v[mid] == x)
                 return mid;
@@ -20,3 +23,23 @@ class Solution{
         return last;
     }
 };
+
+//      TC O(log n)
+int bs(vector<long long> v, long long first, long long last, long long x)
+{
+    if (x < v[0])
+        return -1;
+    if (first > last)
+        return last;
+    int mid = (first + last) / 2;
+    if (v[mid] == x)
+        return mid;
+    else if (v[mid] < x)
+        return bs(v, mid + 1, last, x);
+    else
+        return bs(v, first, mid, x);
+}
+int findFloor(vector<long long> v, long long n, long long x)
+{
+    return bs(v, 0, n - 1, x);
+}
