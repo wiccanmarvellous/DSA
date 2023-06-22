fb46711b47e897506edac7ee171e0097476b1356
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-22 23:57:59 +0530

    feat: create Search_in_Linked_List_GFG.cpp

---
+++ Linked list/Easy 1D/Search_in_Linked_List_GFG.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+};
+
+class Solution {
+  public:
+    bool searchKey(int n, struct Node* head, int key) {
+        Node *temp = head;
+        while (temp != NULL) {
+            if (temp->data == key)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
