ea2526247f594b342cf687ecd26ec6bba0fc3b7f
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-22 22:24:50 +0530

    feat: create Introduction_to_Linked_List_GFG.cpp

---
+++ Linked list/Easy 1D/Introduction_to_Linked_List_GFG.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        Node *head = NULL, *temp = NULL;
+        for (int i = 0; i < arr.size(); i++) {
+            Node *newnode = new Node(arr[i]);
+            if (head == NULL)
+                head = temp = newnode;
+            else {
+                temp->next = newnode;
+                temp = newnode;
+            }
+        }
+        return head;
+    }
+};
