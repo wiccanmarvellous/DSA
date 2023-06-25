8ee4392741e9459db5d6a2ca7bcc82a81f3516c9
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-25 10:18:32 +0530

    create 1 file and update 1 file

---
+++ Linked list/Easy Singly LL/Reverse_Linked_List_leetcode_206.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int val) : val(val), next(nullptr) {}
+    ListNode(int val, ListNode *next) : val(val), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode *t1 = head, *t2 = head->next, *t3 = head->next->next;
+        t1->next = nullptr;        
+        while (t3 != nullptr) {
+            t2->next = t1;
+            t1 = t2;
+            t2 = t3;
+            t3 = t3->next;
+        }
+        t2->next = t1;
+        head = t2;
+        return head;
+    }
+};
--- Linked list/Medium Singly LL/Middle_of_the_Linked_List_leetcode_876.cpp
+++ Linked list/Medium Singly LL/Middle_of_the_Linked_List_leetcode_876.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
@@ -10,20 +10,41 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+// Brute        TC O(n)
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
+    ListNode *middleNode(ListNode *head)
+    {
         ListNode *temp = head;
         int len = 0, i = 0;
-        while (temp != nullptr) {
+        while (temp != nullptr)
+        {
             len++;
             temp = temp->next;
         }
         temp = head;
-        while (i < len / 2) {
+        while (i < len / 2)
+        {
             i++;
             temp = temp->next;
         }
         return temp;
+    }
+};
+
+// Optimal      TC O(n / 2)
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
