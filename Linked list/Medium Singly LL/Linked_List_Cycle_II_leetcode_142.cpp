bb319089bbb21d3e911a4f592b8a6b1de1c3e833
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-26 23:34:37 +0530

    update Linked_List_Cycle_II_leetcode_142.cpp

--- Linked list/Medium Singly LL/Linked_List_Cycle_II_leetcode_142.cpp
+++ Linked list/Medium Singly LL/Linked_List_Cycle_II_leetcode_142.cpp
@@ -1,3 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        unordered_set<ListNode *> set;
+        ListNode *temp = head;
+        while (temp != nullptr)
+        {
+            if (set.find(temp) != set.end())
+                return temp;
+            set.insert(temp);
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                ListNode *entry = head;
+                while (entry != slow) {
+                    entry = entry->next;
+                    slow = slow->next;
+                }
+                return entry;
+            }
+        }
+        return nullptr;
+    }
+};
