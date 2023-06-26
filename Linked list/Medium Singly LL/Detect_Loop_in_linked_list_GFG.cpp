edbcdf671394b0196168a4192bbe792f88907961
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-26 20:23:22 +0530

    feat: create Detect_Loop_in_linked_list_GFG.cpp and Linked_List_Cycle_leetcode_141.cpp

---
+++ Linked list/Medium Singly LL/Detect_Loop_in_linked_list_GFG.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    bool detectLoop(Node *head)
+    {
+        Node *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
