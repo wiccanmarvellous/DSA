da96c97194a1e3127f23925bc1705de1bc8995fa
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-30 20:49:07 +0530

    update Intersection_of_Two_Linked_Lists_leetcode_160.cpp

--- Linked list/Medium Singly LL/Intersection_of_Two_Linked_Lists_leetcode_160.cpp
+++ Linked list/Medium Singly LL/Intersection_of_Two_Linked_Lists_leetcode_160.cpp
@@ -44,3 +44,76 @@ public:
         return nullptr;
     }
 };
+
+// Optimal 1      TC O(m + n)     SC O(1)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *tempA = headA, *tempB = headB;
+        while (tempA != nullptr && tempB != nullptr) {
+            tempB = tempB->next;
+            tempA = tempA->next;
+        }
+        int len = 0;
+        if (tempA == nullptr) {
+            while (tempB != nullptr) {
+                len++;
+                tempB = tempB->next;
+            }
+            tempA = headA;
+            tempB = headB;
+            while (len--)
+                tempB = tempB->next;
+        }
+        else {
+            while (tempA != nullptr) {
+                len++;
+                tempA = tempA->next;
+            }
+            tempA = headA;
+            tempB = headB;
+            while (len--)
+                tempA = tempA->next;
+        }
+        while (tempA != nullptr) {
+            if (tempA == tempB)
+                return tempA;
+            tempA = tempA->next;
+            tempB = tempB->next;
+        }
+        return nullptr;
+    }
+};
+
+// Optimal 2      TC O(m + n)     SC O(1)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *tempA = headA, *tempB = headB;
+        while (tempA != nullptr && tempB != nullptr) {
+            tempB = tempB->next;
+            tempA = tempA->next;
+        }
+        if (tempA == nullptr) {
+            tempA = headB;
+            while (tempB != nullptr) {
+                tempA = tempA->next;
+                tempB = tempB->next;
+            }
+            tempB = headA;
+        }
+        else {
+            tempB = headA;
+            while (tempA != nullptr) {
+                tempA = tempA->next;
+                tempB = tempB->next;
+            }
+            tempA = headB;
+        }
+        while (tempA != tempB) {
+            tempA = tempA->next;
+            tempB = tempB->next;
+        }
+        return tempA;
+    }
+};
