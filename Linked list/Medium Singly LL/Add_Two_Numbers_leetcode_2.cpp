64aedad1c53de3870ef3a431058551dbbc43c0b4
Shreyash <shreyashyaduvanshi@gmail.com>
2023-07-01 11:30:41 +0530

    update Add_Two_Numbers_leetcode_2.cpp

--- Linked list/Medium Singly LL/Add_Two_Numbers_leetcode_2.cpp
+++ Linked list/Medium Singly LL/Add_Two_Numbers_leetcode_2.cpp
@@ -12,22 +12,14 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum = 0, cy = 0;
+        int sum, cy = 0, val1, val2;
         ListNode *t1 = l1, *t2 = l2, *l3 = nullptr, *t3 = nullptr;
         while (t1 != nullptr || t2 != nullptr) {
-            if (t1 == nullptr) {
-                sum = t2->val + cy;
-                t2 = t2->next;
-            }
-            else if (t2 == nullptr) {
-                sum = t1->val + cy;
-                t1 = t1->next;
-            }
-            else {
-                sum = t1->val + t2->val + cy;
-                t1 = t1->next;
-                t2 = t2->next;
-            }
+            if (t1) val1 = t1->val, t1 = t1->next;
+            else val1 = 0;
+            if (t2) val2 = t2->val, t2 = t2->next;
+            else val2 = 0;
+            sum = val1 + val2 + cy;
             ListNode *newnode = new ListNode(sum % 10);
             cy = sum / 10;
             if (l3 == nullptr)
