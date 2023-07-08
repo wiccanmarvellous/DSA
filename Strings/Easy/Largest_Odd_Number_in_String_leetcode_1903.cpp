d1f9c3503e69a3e8699609636d25ac2be76867ab
Shreyash <shreyashyaduvanshi@gmail.com>
2023-07-08 22:14:31 +0530

    update Largest_Odd_Number_in_String_leetcode_1903.cpp

--- Strings/Easy/Largest_Odd_Number_in_String_leetcode_1903.cpp
+++ Strings/Easy/Largest_Odd_Number_in_String_leetcode_1903.cpp
@@ -22,3 +22,14 @@ public:
         return "";
     }
 };
+
+// Optimal      TC O(n)     SC O(1)
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.length() - 1; i >= 0; i--)
+            if ((num[i] - '0') % 2)
+                return num.substr(0, i + 1);
+        return "";
+    }
+};
