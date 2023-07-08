92ba2ca245e16621840157b1d44cae1fb107de3e
Shreyash <shreyashyaduvanshi@gmail.com>
2023-07-08 20:24:50 +0530

    feat: create Largest_Odd_Number_in_String_leetcode_1903.cpp

---
+++ Strings/Easy/Largest_Odd_Number_in_String_leetcode_1903.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute        TC O(n)     SC O(1)
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int maxOdd = 0;
+        string s = "";
+        for (int i = 0; i < num.length(); i++) {
+            for (int j = i; j < num.length(); j++) {
+                s += num[j];
+                string charStr(1, num[j]);
+                int x = stoi(charStr), X = stoi(s);
+                if (x % 2 && X > maxOdd)
+                    maxOdd = X;
+            }
+            s = "";
+        }
+        if (maxOdd)
+            return to_string(maxOdd);
+        return "";
+    }
+};
