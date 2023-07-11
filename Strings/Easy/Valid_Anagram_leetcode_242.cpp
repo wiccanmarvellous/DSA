47275860596a2b1c26a23140e605fcdfa2aa69a9
Shreyash <shreyashyaduvanshi@gmail.com>
2023-07-11 21:16:33 +0530

    update Valid_Anagram_leetcode_242.cpp

--- Strings/Easy/Valid_Anagram_leetcode_242.cpp
+++ Strings/Easy/Valid_Anagram_leetcode_242.cpp
@@ -15,3 +15,34 @@ public:
         return true;
     }
 };
+
+// Optimal      TC O(n)     SC O(n)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> map;
+        for (auto i : s)
+            cout << map[i]++;
+        for (auto i : t)
+            cout << map[i]--;
+        for (auto i : map)
+            if (i.second != 0)
+                return false;
+        return true;
+    }
+};
+
+//  Optimal     TC O(n log n)       SC O(1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.length(); i++)
+            if (s[i] != t[i])
+                return false;
+        return true;
+    }
+};
