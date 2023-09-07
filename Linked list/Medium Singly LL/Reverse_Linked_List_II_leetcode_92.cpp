d9ffd8e59f69f79a20603a0f59de6855c008767e
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-07 11:41:25 +0530

    update Reverse_Linked_List_II_leetcode_92.cpp

--- Linked list/Medium Singly LL/Reverse_Linked_List_II_leetcode_92.cpp
+++ Linked list/Medium Singly LL/Reverse_Linked_List_II_leetcode_92.cpp
@@ -33,3 +33,48 @@ public:
         return dummy->next;
     }
 };
+
+
+class Solution {
+public:
+
+    ListNode *reverse(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode *prev = NULL, *curr = head;
+        while (curr != NULL) {
+            ListNode *nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode *start_prev = NULL, *start = NULL, *end = NULL, *end_next = NULL;
+        ListNode *curr = head;
+        int i = 1;
+        while (curr != NULL) {
+            if (i < left)
+                start_prev = curr;
+            if (i == left)
+                start = curr;
+            if (i == right)
+                end = curr;
+            curr = curr->next;
+            i++;
+        }       
+        end_next = end->next;
+        end->next = NULL;
+        end = reverse(start);
+        if (start_prev == NULL) {
+            head = end;
+        }
+        else {
+            start_prev->next = end;
+        }
+        start->next = end_next;
+        return head;
+    }
+};
