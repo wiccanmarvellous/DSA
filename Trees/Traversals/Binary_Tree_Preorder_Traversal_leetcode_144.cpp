762876ac97d5df73c95320a0e1632ad774c6736d
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-10 20:59:44 +0530

    create 1 file and move 1 file

---
+++ Trees/Traversals/Binary_Tree_Preorder_Traversal_leetcode_144.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//  TC O(n)     SC O(n)
+class Solution {
+public:
+    void helper(TreeNode *root, vector<int> &ans) {
+        if (root == NULL)
+            return;
+        ans.push_back(root->val);
+        helper(root->left, ans);
+        helper(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root, ans);
+        return ans;
+    }
+};
