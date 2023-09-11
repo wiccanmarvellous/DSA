44e84d56890131aed4e17845775b0d6c367ad4ba
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-11 22:04:00 +0530

    update Implement_Stack_using_Linked_List_GFG.cpp

--- Stack and Queue/Learning/Implement_Stack_using_Linked_List_GFG.cpp
+++ Stack and Queue/Learning/Implement_Stack_using_Linked_List_GFG.cpp
@@ -1,20 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 
 
 //Function to push an integer into the stack.
-void MyStack ::push(int x) 
-{
-    StackNode *node = new StackNode(x);
-    if (top != NULL)
-        node->next = top;
-    top = node;
-}
+// void MyStack ::push(int x) 
+// {
+//     StackNode *node = new StackNode(x);
+//     if (top != NULL)
+//         node->next = top;
+//     top = node;
+// }
 
-//Function to remove an item from top of the stack.
-int MyStack ::pop() 
-{
-    if (top == NULL)
-        return -1;
-    int ans = top->data;
-    top = top->next;
-    return ans;
-}
+// //Function to remove an item from top of the stack.
+// int MyStack ::pop() 
+// {
+//     if (top == NULL)
+//         return -1;
+//     int ans = top->data;
+//     top = top->next;
+//     return ans;
+// }
