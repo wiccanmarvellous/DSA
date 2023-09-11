7308b7f30808d27e913fc788708479bae6561331
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-11 20:32:04 +0530

    update Implement_Stack_using_Queues_leetcode_225.cpp

--- Stack and Queue/Learning/Implement_Stack_using_Queues_leetcode_225.cpp
+++ Stack and Queue/Learning/Implement_Stack_using_Queues_leetcode_225.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// stack using 2 queues
 class MyStack {
 public:
     queue<int> q1, q2;
@@ -29,6 +31,47 @@ public:
     
     bool empty() {
         return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+// stack using 1 queue
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int ans = q.front();
+        q.pop();
+        return ans;
+    }
+    
+    int top() {
+        int i = q.front();
+        return i;
+    }
+    
+    bool empty() {
+        return q.empty();
     }
 };
 
