dce225848f56a86c74bf9df9fb96d87d4e71c8dd
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-19 14:56:16 +0530

    feat: create Diameter_of_Binary_Tree_leetcode_543.cpp

---
+++ Trees/Medium Problems/Diameter_of_Binary_Tree_leetcode_543.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int helper(TreeNode* root, int &ans) {
+        if (root == NULL)
+            return 0;
+        int l = helper(root->left, ans);
+        int r = helper(root->right, ans);
+        ans = max(ans, (l + r));
+        return max(l, r) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        helper(root, ans);
+        return ans;
+    }
+};
