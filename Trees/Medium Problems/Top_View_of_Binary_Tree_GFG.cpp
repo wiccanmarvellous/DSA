617aa3080f62a9abd6a085a62d2ed215ab24b96f
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-29 22:22:13 +0530

    feat: create Top_View_of_Binary_Tree_GFG.cpp

---
+++ Trees/Medium Problems/Top_View_of_Binary_Tree_GFG.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        queue<pair<Node*, int>> q;
+        map<int, int> dict;
+        q.push({root, 0});
+        while (!q.empty()) {
+            Node *node = q.front().first;
+            int idx = q.front().second;
+            q.pop();
+            if (dict.find(idx) == dict.end())
+                dict[idx] = node->data;
+            if (node->left) q.push({node->left, idx - 1});
+            if (node->right) q.push({node->right, idx + 1});
+        }
+        vector<int> ans;
+        for (auto i : dict)
+            ans.push_back(i.second);
+        return ans;
+    }
+
+};
