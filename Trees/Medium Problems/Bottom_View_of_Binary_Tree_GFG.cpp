1cbac3415e3fd4acac3e360d2b338dd6c5edebd5
Shreyash <shreyashyaduvanshi@gmail.com>
2023-09-29 22:27:57 +0530

    feat: create Bottom_View_of_Binary_Tree_GFG.cpp

---
+++ Trees/Medium Problems/Bottom_View_of_Binary_Tree_GFG.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        queue<pair<Node*, int>> q;
+        map<int, int> dict;
+        q.push({root, 0});
+        while (!q.empty()) {
+            Node *node = q.front().first;
+            int idx = q.front().second;
+            q.pop();
+            dict[idx] = node->data;
+            if (node->left) q.push({node->left, idx - 1});
+            if (node->right) q.push({node->right, idx + 1});
+        }
+        vector<int> ans;
+        for (auto i : dict)
+            ans.push_back(i.second);
+        return ans;
+    }
+};
