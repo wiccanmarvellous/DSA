b72557d63847448cda92bd0af5d57983abf6e20d
Shreyash <shreyashyaduvanshi@gmail.com>
2024-03-18 00:07:26 +0530

    update Frog_Jump_Coding_Ninjas.cpp

--- Dynamic Programming/1D DP/Frog_Jump_Coding_Ninjas.cpp
+++ Dynamic Programming/1D DP/Frog_Jump_Coding_Ninjas.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Memoization
-#include <bits/stdc++.h> 
+//Memoization   TC O(n) SC O(n)
 int helper(int n, vector<int> &heights, vector<int> &dp) {
     if (n == 0)
         return 0;
@@ -21,8 +20,7 @@ int frogJump(int n, vector<int> &heights)
    return helper (n - 1, heights, dp);
 }
 
-//Tabulation
-#include <bits/stdc++.h> 
+//Tabulation    TC O(n) SC O(n)
 int frogJump(int n, vector<int> &heights)
 {
     vector<int> dp(n, 0);
@@ -35,4 +33,20 @@ int frogJump(int n, vector<int> &heights)
         dp[i] = min(one, two);
     }
     return dp[n - 1];
+}
+
+//  TC O(n) SC O(1)
+int frogJump(int n, vector<int> &heights)
+{
+    int prev1 = 0, prev2 = 0;
+    for (int i = 1; i < n; i++) {
+        int one = prev1 + abs(heights[i] - heights[i - 1]);
+
+        int two = INT_MAX;
+        if (i > 1) two = prev2 + abs(heights[i] - heights[i - 2]);
+        int curr = min(one, two);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
 }
